529479e17315be4b4f032714ed172cf45359d8fb
Paul Franco <mail@paulfran.co>
2020-09-20 10:21:46 -0700

    fixed bug

--- app/src/main/java/co/paulfran/mvvmshoppinglist/ui/shoppinglist/AddShoppingItemDialog.kt
+++ app/src/main/java/co/paulfran/mvvmshoppinglist/ui/shoppinglist/AddShoppingItemDialog.kt
@@ -2,6 +2,7 @@ package co.paulfran.mvvmshoppinglist.ui.shoppinglist
 
 import android.content.Context
 import android.os.Bundle
+import android.view.Window
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatDialog
 import co.paulfran.mvvmshoppinglist.R
@@ -11,7 +12,8 @@ import kotlinx.android.synthetic.main.dialog_add_shopping_item.*
 class AddShoppingItemDialog(context: Context, var addDialogListener: AddDialogListener) : AppCompatDialog(context) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_shopping)
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE)
+        setContentView(R.layout.dialog_add_shopping_item)
 
         tvAdd.setOnClickListener {
             val name = etName.text.toString()
